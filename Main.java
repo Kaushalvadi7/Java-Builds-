6e20d587fa882d18a4ff43f0cb9d4e47947442b0
kaushal <kaushalvadi7777@gmail.com>
2025-03-17 09:54:42 +0530

    Initial commit: My First code

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println();
+
+        char name = 'k';
+        System.out.println(name);
+        System.out.println();
+
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        System.out.println();
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+    }
+}
